3c82646eed9d3cb2ca659f984efa7093f72e61ab
Lucas Oliveira <lcastro.oliveira@gmail.com>
2017-06-16 17:27:04 -0300

    Fix Factory urls format

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :contact do
     email 'interested_user@gmail.com'
     ip '198.897.987.1'
-    urls "{pages: [{ url: '/sobre.html', timestamp: '2017-06-16T12:18:01.517Z'}]}"
+    urls pages: [{ url: '/sobre.html', timestamp: '2017-06-16T12:18:01.517Z'} ]
   end
 end
